940b91483575b1cd6eb35d19ecb7780bee4e1502
Chart <Chartfizz@outlook.com>
2022-12-09 17:25:10 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,34 @@
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+
+  int number;
+  
+  int odd = 0;
+  int even = 0;
+    
+    while (number != 0 )
+        {
+          cout << "Enter an integer: ";
+          cin >> number;
+          if (number%2 == 0 && number != 0)
+            {
+            
+            even++;
+            }
+          else if (number%2 != 0)
+          
+          
+            {
+            
+            odd++;
+            }
+          
+        }
+
+        cout << "#Even numbers = " << even << endl ;
+
+        cout << "#Odd numbers = " << odd << endl ;
+
     return 0;
 }
